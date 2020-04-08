7c637ab3e1e73fba74eca3c2e5cd97fbe45a8f8f
Pierre Arnaud <arnaud@epsitec.ch>
2020-04-08 09:04:41 +0200

    Update hugo scripts

--- hugo-new.bat
+++ hugo-new.bat
@@ -1,3 +1,3 @@
 pushd site
-C:\tools\hugo.exe new post/2020/02/_.md
+C:\tools\hugo.exe new post/2020/04/_.md
 popd
--- hugo-run.bat
+++ hugo-run.bat
@@ -1,4 +1,4 @@
 pushd site
-start C:\tools\hugo.exe server -w --disableFastRender --port 1414
-start http://localhost:1414
+start C:\tools\hugo.exe server -w --disableFastRender --port 8432
+start http://localhost:8432
 popd
